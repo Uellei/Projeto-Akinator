14e5be9d2a2d0a45aaee8a485a67ef57cc03a643
AndreKenji1 <149121429+AndreKenji1@users.noreply.github.com>
2023-11-09 13:02:16 -0300

    Update AkinatorGame.java

--- AkinatorGame.java
+++ AkinatorGame.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class AkinatorGame {
     static Map<String, Map<String, String>> possiveisRestaurantes;
+
     public static void main(String[] args) {
         Estabelecimento restaurant = new Estabelecimento();
         possiveisRestaurantes = restaurant.getPossiveisRestaurantes();
@@ -16,10 +17,10 @@ public class AkinatorGame {
             if (resposta.equals("sim")) {
                 System.out.print("O País de Origem é os Estados Unidos? ");
                 resposta = scanner.nextLine().toLowerCase();
-                if(resposta.equals("sim")) {
+                if (resposta.equals("sim")) {
                     System.out.println("Você está pensando no OutBack!!");
                     break;
-                } else if(resposta.equals("nao")) {
+                } else if (resposta.equals("nao")) {
                     System.out.println("Você está pensando no Paris 6!!");
                     break;
                 } else {
@@ -28,31 +29,31 @@ public class AkinatorGame {
             } else if (resposta.equals("nao")) {
                 System.out.print("O País de Origem é os Estados Unidos? ");
                 resposta = scanner.nextLine().toLowerCase();
-                if(resposta.equals("sim")) {
+                if (resposta.equals("sim")) {
                     System.out.print("A influência culinária é Americana? ");
                     resposta = scanner.nextLine().toLowerCase();
-                    if(resposta.equals("sim")) {
+                    if (resposta.equals("sim")) {
                         System.out.print("O Prato Principal é o Hamburguer? ");
                         resposta = scanner.nextLine().toLowerCase();
-                        if(resposta.equals("sim")) {
+                        if (resposta.equals("sim")) {
                             System.out.print("O Big Mac faz parte do cardápio? ");
                             String resposta6 = scanner.nextLine().toLowerCase();
-                            if(resposta6.equals("sim")) {
+                            if (resposta6.equals("sim")) {
                                 System.out.println("Você está pensando no Mc'Donalds!!");
                                 break;
-                            } else if(resposta6.equals("nao")) {
+                            } else if (resposta6.equals("nao")) {
                                 System.out.println("Você está pensando no Burguer King!!");
                                 break;
                             } else {
                                 System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
                             }
-                        } else if(resposta.equals("nao")) {
+                        } else if (resposta.equals("nao")) {
                             System.out.print("O Prato Principal é o Frango Frito? ");
                             resposta = scanner.nextLine().toLowerCase();
-                            if(resposta.equals("sim")) {
+                            if (resposta.equals("sim")) {
                                 System.out.println("Você está pensando no KFC!!");
                                 break;
-                            } else if(resposta.equals("nao")) {
+                            } else if (resposta.equals("nao")) {
                                 System.out.println("Você está pensando no Subway!!");
                                 break;
                             } else {
@@ -61,19 +62,28 @@ public class AkinatorGame {
                         } else {
                             System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
                         }
-                    } else if(resposta.equals("nao")) {
+                    } else if (resposta.equals("nao")) {
                         System.out.print("A Influência Culinária é Mexicana? ");
                         resposta = scanner.nextLine().toLowerCase();
-                        if(resposta.equals("sim")) {
+                        if (resposta.equals("sim")) {
                             System.out.println("Você está pensando no Taco Bell!!");
                             break;
-                        } else if(resposta.equals("nao")) {
+                        } else if (resposta.equals("nao")) {
                             System.out.print("A Influência é Italiana? ");
                             resposta = scanner.nextLine().toLowerCase();
-                            if(resposta.equals("sim")) {
-                                System.out.println("Você está pensando na Domino's!!");
-                                break;
-                            } else if(resposta.equals("nao")) {
+                            if (resposta.equals("sim")) {
+                                System.out.print("O Logo tem um dominó? ");
+                                resposta = scanner.nextLine().toLowerCase();
+                                if (resposta.equals("sim")) {
+                                    System.out.println("Você está pensando na Domino's!!");
+                                    break;
+                                } else if (resposta.equals("nao")) {
+                                    System.out.println("Você está pensando no Pizza Hut!!");
+                                    break;
+                                } else {
+                                    System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
+                                }
+                            } else if (resposta.equals("nao")) {
                                 System.out.println("Você está pensando no Starbucks!!");
                                 break;
                             } else {
@@ -83,31 +93,31 @@ public class AkinatorGame {
                             System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
                         }
                     }
-                } else if(resposta.equals("nao")) {
+                } else if (resposta.equals("nao")) {
                     System.out.print("O País de Origem é o Brasil? ");
                     resposta = scanner.nextLine().toLowerCase();
-                    if(resposta.equals("sim")) {
+                    if (resposta.equals("sim")) {
                         System.out.print("O Prato Principal é Esfiha? ");
                         resposta = scanner.nextLine().toLowerCase();
-                        if(resposta.equals("sim")) {
+                        if (resposta.equals("sim")) {
                             System.out.print("O Logo é um gênio? ");
                             resposta = scanner.nextLine().toLowerCase();
-                            if(resposta.equals("sim")) {
+                            if (resposta.equals("sim")) {
                                 System.out.println("Você está pensando no Habib's!!");
                                 break;
-                            } else if(resposta.equals("nao")) {
+                            } else if (resposta.equals("nao")) {
                                 System.out.println("Você está pensando no Ponto da Esfiha!!");
                                 break;
                             } else {
                                 System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
                             }
-                        } else if(resposta.equals("nao")) {
+                        } else if (resposta.equals("nao")) {
                             System.out.print("O Prato Principal é Massa");
                             resposta = scanner.nextLine().toLowerCase();
-                            if(resposta.equals("sim")) {
+                            if (resposta.equals("sim")) {
                                 System.out.println("Você está pensando no Spoletto!!");
                                 break;
-                            } else if(resposta.equals("nao")) {
+                            } else if (resposta.equals("nao")) {
                                 System.out.println("Você está pensando no Bob's!!");
                                 break;
                             } else {
@@ -116,7 +126,7 @@ public class AkinatorGame {
                         } else {
                             System.out.println("Resposta Inválida. Por favor, responda com 'Sim' ou 'Não'");
                         }
-                    } else if(resposta.equals("nao")) {
+                    } else if (resposta.equals("nao")) {
                         System.out.println("Você está pensando no Pizza Hut!!");
                         break;
                     }
